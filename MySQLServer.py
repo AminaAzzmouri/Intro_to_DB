0992d35dd83da3db4e4085d654504f30b452f24f
Amina Azzmouri <azzmouriamina@gmail.com>
2025-06-29 02:24:49 +0100

    Fix exception handling to explicitly catch mysql.connector.Error as required by autochecker

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,6 +1,5 @@
 import os
 import mysql.connector
-from mysql.connector import Error
 
 try:
     # Get MySQL password from environment variable named MYSQL_PASSWORD
@@ -18,7 +17,7 @@ try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error while connecting to MySQL: {e}")
 
 finally:
