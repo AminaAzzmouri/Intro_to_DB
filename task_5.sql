3e4bd2aaf02322dd5458773349cb2d0e01a2f550
Amina Azzmouri <azzmouriamina@gmail.com>
2025-06-29 02:59:34 +0100

    Updated task_5.sql file

--- task_5.sql
+++ task_5.sql
@@ -1,10 +1,2 @@
-CREATE TABLE Customer (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215),
-    address TEXT
-);
-
-
 INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
